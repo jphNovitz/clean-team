8f5e17b5a52f358e0a3952050ce2b2a62cc0a48a
Novitz Jean-Philippe <novitz@gmail.com>
2022-08-29 04:15:15 +0200

    new (components) header components from h1 to h6

---
+++ resources/js/Components/Titles/Header3.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="font-semibold text-lg text-gray-custom leading-tight">
+        <slot />
+    </h1>
+</template>
---
+++ resources/js/Components/Titles/Header5.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="font-semibold text-sm text-gray-custom leading-tight">
+        <slot />
+    </h1>
+</template>
