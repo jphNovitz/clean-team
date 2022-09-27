01b2d2fdce3969a6632ccf3ba7bf006a28fa736c
Novitz Jean-Philippe <novitz@gmail.com>
2022-09-27 05:01:23 +0200

    Refactor missing products list for desktop and mobile

--- resources/js/Pages/MissingProducts/Partials/ProductLine.vue
+++ resources/js/Pages/MissingProducts/Partials/ProductLine.vue
@@ -5,8 +5,15 @@ import { usePage } from '@inertiajs/inertia-vue3'
 import { reactive } from 'vue'
 import { trans } from "matice";
 import { useForm } from '@inertiajs/inertia-vue3'
+// import moment from "moment" ;
+// import 'moment/locale/fr' ;
+import moment from 'moment-with-locales-es6';
+
 
 import Button from '@/Components/Button.vue';
+// import ArrowPath from '@heroicons/vue/24/solid'
+import { BeakerIcon } from '@heroicons/vue/20/solid'
+import { ArrowPathIcon } from '@heroicons/vue/20/solid'
 import JetInput from '@/Components/Input.vue';
 import JetActionMessage from '@/Components/ActionMessage.vue';
 
@@ -16,6 +23,7 @@ const props = defineProps({
     message: String
 });
 
+// moment = moment.locale('fr')
 const form = useForm({
     id: props.line.id,
     active: props.line.active,
@@ -40,29 +48,38 @@ function product(id) {
 
 <template>
     {{props.message}}
-    <div class="ml-4 w-full my-12">
-
+    <div class="w-full py-2 px-2 flex items-center py-2.5 md:py-1  border-t border-t-slate-200">
+        
         <form @submit.prevent="form.post(route('missing_products_test'), {
           preserveScroll: true,
           onSuccess: () => alert.success = true,
-        })" class="w-full grid grid-cols-12 gap-8 align-middle">
+        })" class="w-full items-center grid grid-cols-2 md:grid-cols-12 gap-6">
 
-            <div v-if="product(form.product_id).type_id == 1" class="col-span-1">
-                <JetInput type="number" class="w-16" v-model.number="form.quantity" />
-            </div>
-            <div v-else class="col-span-1">
-                <input type="checkbox" v-model.checkbox="form.reported">
-            </div>
-            <div class="col-span-5">
+            <div class="col-span-2 md:col-span-5 flex items-center text-xl md:text-lg">
                 {{product(form.product_id).name}}
+            </div>
+            <div class="col-span-1 md:col-span-2">
+                <JetInput v-if="product(form.product_id).type_id == 1"  
+                type="number" 
+                class="w-3/4 " 
+                v-model.number="form.quantity" />
+                <input v-else type="checkbox" v-model.checkbox="form.reported">
+            </div>
+            <!-- <div class="col-span-1">
                 ({{product(form.product_id).type_id}})
+            </div> -->
+            <div class="col-span-1 md:col-span-1">
+                <Button type="submit" :disabled="form.processing"> <ArrowPathIcon class="w-4" /> </Button>
             </div>
-            <div class="col-span-4">
-                <Button type="submit" :disabled="form.processing"> {{trans('btn.Save')}} </Button>
+            <div class="hidden md:grid md:col-span-2">
+                {{ moment(props.line.created_at).format("l") }}
             </div>
-            <div class="col-span-2">
-                <JetActionMessage :on="alert.success" > {{trans('app.Saved')}}</JetActionMessage>
+            <div class="hidden md:grid md:col-span-2">
+                {{ moment(props.line.updated_at).format("l") }}
             </div>
+            <!-- <div class="col-span-2">
+                <JetActionMessage :on="alert.success"> {{trans('app.Saved')}}</JetActionMessage>
+            </div> -->
         </form>
 
     </div>
--- resources/js/Pages/MissingProducts/Show.vue
+++ resources/js/Pages/MissingProducts/Show.vue
@@ -17,7 +17,7 @@ const journal = usePage().props.value.journal
     <AppLayout title="Desgin System">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Products  {{props.message}}
+                Products
             </h2>
         </template>
 
@@ -33,10 +33,16 @@ const journal = usePage().props.value.journal
                     </template>
 
                     <template #content>
-
-                        <div v-for="line in journal" :key="line.id" class="flex items-center justify-between">
+                        <div class="hidden w-full items-center md:grid md:grid-cols-12 gap-6 bg-indigo-200 text-indigo-900 py-2 px-2">
+                            <div class="md:col-span-5">Nom </div>
+                            <div class="md:col-span-2"> </div>
+                            <div class="md:col-span-1"> </div>
+                            <div class="md:col-span-2"> Création </div>
+                            <div class="md:col-span-2"> Modification </div>
+                        </div>
+                        <div v-for="line in journal" :key="line.id" class="flex items-center justify-between odd:bg-indigo-50">
                             <ProductLine :line="line"></ProductLine>
-                            
+
 
                         </div>
                     </template>
