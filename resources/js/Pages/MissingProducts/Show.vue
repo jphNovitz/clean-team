0214bb19165c20994a339e973ff607ab38646494
Novitz Jean-Philippe <novitz@gmail.com>
2022-09-29 03:56:47 +0200

    refactor show linen and productts - simplification - new switch button

--- resources/js/Pages/MissingProducts/Show.vue
+++ resources/js/Pages/MissingProducts/Show.vue
@@ -11,6 +11,7 @@ import { trans } from "matice";
 const journal = usePage().props.value.journal
 const products = usePage().props.value.initialProducts
 const showConsumable = ref(false)
+const showLinens = ref(true)
 const showOddColor = ref(false)
 
 
@@ -18,20 +19,22 @@ function typeID(productID) {
     return products.filter(product => product.id == productID)[0].type_id
 }
 
-function showComponent(typeID, productID) {
-    if (typeID === 2) {
-        if (products.filter(product => product.id == productID)[0].type_id === 2) return showConsumable.value
-        else return true
-    }
+function showComponent(productID) { 
+    switch(products.filter(product => product.id == productID)[0].type_id){
+        case 1:
+            return showLinens.value
+        case 2:
+            return showConsumable.value
+    }  
 }
 
-watch(showConsumable, async () => { showOddColor.value = showConsumable.value})
+watch([showConsumable, showLinens], async () => showOddColor.value = (showConsumable.value === showLinens.value))
 
 
 </script>
     
 <template>
-    <AppLayout title="Desgin System">
+    <AppLayout title="Produits manquants">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Products
@@ -43,9 +46,6 @@ watch(showConsumable, async () => { showOddColor.value = showConsumable.value})
                 <JetActionSection class="mt-10 sm:mt-0">
                     <template #title>
                         Missing Products <div class="flex justify-center">
-
-
-
                         </div>
                     </template>
 
@@ -55,14 +55,23 @@ watch(showConsumable, async () => { showOddColor.value = showConsumable.value})
 
                     <template #content>
                         <div class="flex justify-start my-4">
-                            <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
-                                <input type="checkbox" value="" id="default-toggle" v-model="showConsumable"
+                            <label for="default-consumable" class="inline-flex relative items-center cursor-pointer">
+                                <input type="checkbox" value="" id="default-consumable" v-model="showConsumable"
                                     class="sr-only peer">
                                 <div
                                     class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
                                 </div>
                                 <span class="ml-3 text-sm font-medium text-gray-900 dark:text-indigo-600">Afficher
                                     Produits</span>
+                            </label>
+                            <label for="default-linens" class="inline-flex relative items-center cursor-pointer">
+                                <input type="checkbox" value="" id="default-linens" v-model="showLinens"
+                                    class="sr-only peer">
+                                <div
+                                    class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                                </div>
+                                <span class="ml-3 text-sm font-medium text-gray-900 dark:text-indigo-600">Afficher
+                                    Linge</span>
                             </label>
                         </div>
                         <div
@@ -73,9 +82,10 @@ watch(showConsumable, async () => { showOddColor.value = showConsumable.value})
                             <!-- <div class="hidden 2xl:grid 2xl:col-span-2"> Création </div> -->
                             <div class="hidden md:grid md:col-span-2"> Modification </div>
                         </div>
-                        <div v-for="line in journal" :key="line.id" class="flex items-center justify-between transition-colors ease-in-out delay-150 duration-1000"
+                        <div v-for="line in journal" :key="line.id"
+                            class="flex items-center justify-between transition-colors ease-in-out delay-150 duration-1000"
                             :class="{'odd:bg-indigo-50' : showOddColor}">
-                            <ProductLine :line="line" v-if="showComponent(2, line.product_id)"></ProductLine>
+                            <ProductLine :line="line" v-if="showComponent(line.product_id)"></ProductLine>
                         </div>
                     </template>
 
