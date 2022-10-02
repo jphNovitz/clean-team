702d3a6cb54a22f19e0cd5383ef22a0f7dc33b9b
Novitz Jean-Philippe <novitz@gmail.com>
2022-10-02 22:16:58 +0200

    rename from Product Line to Journal Line new ProductLine bases an Journal Line

--- resources/js/Pages/MissingProducts/Partials/JournalLine.vue
+++ resources/js/Pages/MissingProducts/Partials/JournalLine.vue
@@ -15,21 +15,21 @@ import { BeakerIcon } from '@heroicons/vue/20/solid'
 import { ArrowPathIcon } from '@heroicons/vue/20/solid'
 import JetLabel from '@/Components/Label.vue';
 import JetInput from '@/Components/Input.vue';
-import Checkbox from '@/Components/Checkbox.vue';
 import JetActionMessage from '@/Components/ActionMessage.vue';
-// import Checkbox from '../../../../../vendor/laravel/jetstream/stubs/inertia/resources/js/Components/Checkbox.vue';
 
 
 const props = defineProps({
-    product: Object,
+    line: Object,
     message: String
 });
 
+// moment = moment.locale('fr')
 const form = useForm({
-    id: props.product.id,
-    name: props.product.name,
-    default: props.product.default,
-    type_id: props.product.type_id
+    id: props.line.id,
+    active: props.line.active,
+    reported: props.line.reported,
+    quantity: props.line.quantity,
+    product_id: props.line.product_id,
 })
 
 const products = usePage().props.value.initialProducts
@@ -39,53 +39,49 @@ const alert = reactive({
 })
 
 
-// function product(id) {
-//     return products.filter(product => product.id == id)[0]
-// }
+function product(id) {
+    return products.filter(product => product.id == id)[0]
+}
 
 watch(alert, async () => {
-    if (alert.success) setTimeout(function () { alert.message = '', alert.success = false }, 3000);
+    if (alert.success) setTimeout(function() { alert.message = '', alert.success = false }, 3000);
 })
 </script>
 
 <template>
-
-    <div
-        class="w-full py-2 px-2 flex justify-center items-center  md:py-1  border-t border-t-indigo-50  border-b border-b-indigo-50 ">
-
+   
+    <div class="w-full py-2 px-2 flex items-center  md:py-1  border-t border-t-indigo-50  border-b border-b-indigo-50 ">
+        
         <form @submit.prevent="form.post(route('missing_products_test'), {
           preserveScroll: true,
           onSuccess: () => alert.success = true,
-        })" class="w-full items-center grid grid-cols-3 md:grid-cols-12 gap-6">
+        })" class="w-full items-center grid grid-cols-2 md:grid-cols-12 gap-6">
 
-            <div class="col-span-3 md:col-span-5  items-start text-xl md:text-lg flex flex-col  ">
+            <div class="col-span-2 md:col-span-5  items-start text-xl md:text-lg flex flex-col  ">
                 <!-- <div class="md:hidden text-sm text-gray-700 "> {{trans('auth.Name')}}</div> -->
                 <JetLabel class="md:hidden"> {{trans('auth.Name')}} </JetLabel>
-                {{product.name}}
+                 {{product(form.product_id).name}} 
             </div>
             <div class="col-span-1 md:col-span-2 ">
-                <JetLabel class="md:hidden"> Type </JetLabel>
-                <select v-model="form.type_id"
-                    class="w-full text-sm p-0  rounded-md border-0  outline outline-offset-2 outline-1 outline-slate-00">
-                    <option class="text-sm" value="1">Linge</option>
-                    <option class="text-sm" value="2">Produit</option>
-                </select>
+                <JetLabel class="md:hidden">{{trans('app.Quantity')}}</JetLabel>     
+                <JetInput v-if="product(form.product_id).type_id == 1"  
+                type="number" 
+                class="w-3/4 " 
+                v-model.number="form.quantity" />
+                <input v-else type="checkbox" v-model.checkbox="form.reported">
             </div>
+            <!-- <div class="col-span-1">
+                ({{product(form.product_id).type_id}})
+            </div> -->
             <div class="col-span-1 md:col-span-1">
-                <JetLabel class="md:hidden"> Par défaut </JetLabel>
-                <Checkbox v-model="form.default" />
-            </div>
-            <div class="col-span-1 md:col-span-2">
-                <Button type="submit" :disabled="form.processing">
-                    <ArrowPathIcon class="w-4" />
-                </Button>
+                <Button type="submit" :disabled="form.processing"> <ArrowPathIcon class="w-4" /> </Button>
             </div>
             <!-- <div class="hidden 2xl:grid md:col-span-2">
                 {{ moment(props.line.created_at).format("l") }}
             </div> -->
-            <!-- <div class="hidden md:grid md:col-span-2">
-                {{ moment(props.product.updated_at).format("l") }}
-            </div> -->
+            <div class="hidden md:grid md:col-span-2">
+                {{ moment(props.line.updated_at).format("l") }}
+            </div>
             <div class="col-span-2">
                 <JetActionMessage :on="alert.success"> {{trans('app.Saved')}}</JetActionMessage>
             </div>
