d8f2e04e255a6405f1e518d2bf91678d2c41c618
Novitz Jean-Philippe <novitz@gmail.com>
2022-10-16 23:33:40 +0200

    new (route) - products_not_in_journal - addProductInJournal

--- routes/web.php
+++ routes/web.php
@@ -37,10 +37,11 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
     Route::get('/missing', [MissingProductsController::class, 'index'])->middleware('share_products')->name('missing_products');
+    Route::get('', [MissingProductsController::class, 'productsNotInJournal'])->name('products_not_in_journal');
+    Route::put('', [MissingProductsController::class, 'addProductInJournal'])->name('add_product_in_journal');
     Route::post('', [MissingProductsController::class, 'updateLine'])->middleware('share_products')->name('missing_products_test');
     Route::post('', [ProductController::class, 'create'])->name('create_product');
     Route::put('', [ProductController::class, 'update'])->middleware('share_products')->name('update_product');
-    Route::get('/design', function () {
-        ;
+    Route::get('/design', function () {;
     })->name('design');
 });
