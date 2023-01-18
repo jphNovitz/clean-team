6058c077a9c5f53acd90129a2ba3da06873bc401
jphNovitz <novitz@gmail.com>
2023-01-18 23:39:44 +0100

    change files / class names to be complient with psr-4

--- database/factories/MissingJournalProductfactory.php
+++ database/factories/MissingJournalProductfactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MissingProducts>
  */
-class MissingProductsFactory extends Factory
+class MissingJournalProductfactory extends Factory
 {
     /**
      * Define the model's default state.
