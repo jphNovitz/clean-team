ff0359579e3d76d67697171e2be40e42835c7f5b
jphNovitz <novitz@gmail.com>
2023-01-23 18:39:53 +0100

    refactor SecondaryButton.vue component for border / ring color on hover et focus

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 m-1 bg-transparent  text-tertiary dark:text-white-custom p-2 ring-2 ring-offset-1 ring-tertiary border-0 border-tertiary dark:border-white-custom rounded-sm font-semibold text-xs uppercase tracking-widest hover:bg-red-100  hover:text-tertiary active:text-white-custom active:bg-red-300 focus-visible:none focus:text-tertiary focus:outline-none focus:border-red-100 focus:ring focus:ring-gray-300 disabled:opacity-25 transition mx-3">
+    <button :type="type" class="inline-flex items-center px-4 py-2 m-1 bg-transparent  text-tertiary dark:text-white-custom p-2 ring-2 ring-offset-1 ring-tertiary  rounded-sm font-semibold text-xs uppercase tracking-widest hover:bg-red-100  hover:text-tertiary active:text-white-custom active:bg-red-300 focus-visible:none focus:text-tertiary focus:outline-none focus:ring focus:ring-gray-300 dark:hover:bg-gray-700 dark:hover:text-white-custom dark:focus:bg-gray-800 dark:focus:text-tertiary border-0 border-tertiary dark:ring-gray-700 focus:border-0 focus:ring-offset-transparent dark:focus:ring-tertiary disabled:opacity-25 transition mx-3 ">
         <slot />
     </button>
 </template>
